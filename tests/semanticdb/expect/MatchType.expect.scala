56f8fc8fd1c721932388346c960204e3f7976370
tanishiking <rikiriki1238@gmail.com>
2021-07-05 17:50:39 +0900

    updateExpect

--- tests/semanticdb/expect/MatchType.expect.scala
+++ tests/semanticdb/expect/MatchType.expect.scala
@@ -1,11 +1,10 @@
 
 
-type Elem/*<-_empty_::MatchType$package.Elem#*/[X/*<-_empty_::MatchType$package.Elem#[X]*/] = X/*->_empty_::MatchType$package.Elem#[X]*/ match
+type Elem/*<-_empty_::MatchType$package.Elem#*/[X/*<-_empty_::MatchType$package.Elem#[X]*/] <: Any/*->scala::Any#*/ = X/*->_empty_::MatchType$package.Elem#[X]*/ match
   case String/*->scala::Predef.String#*/ => Char/*->scala::Char#*/
   case Array/*->scala::Array#*/[t/*<-local0*/] => t/*->local0*/
-  case Iterable/*->scala::package.Iterable#*/[t/*<-local1*/] => t/*->local1*/
 
-type Concat/*<-_empty_::MatchType$package.Concat#*/[Xs/*<-_empty_::MatchType$package.Concat#[Xs]*/ <: Tuple/*->scala::Tuple#*/, +Ys/*<-_empty_::MatchType$package.Concat#[Ys]*/ <: Tuple/*->scala::Tuple#*/] <: Tuple/*->scala::Tuple#*/ = Xs/*->_empty_::MatchType$package.Concat#[Xs]*/ match
-  case EmptyTuple/*->scala::Tuple$package.EmptyTuple#*/ => Ys/*->_empty_::MatchType$package.Concat#[Ys]*/
-  case x/*<-local2*/ *:/*->scala::`*:`#*/ xs/*<-local3*/ => x/*->local2*/ *:/*->scala::`*:`#*/ Concat/*->_empty_::MatchType$package.Concat#*/[xs/*->local3*/, Ys/*->_empty_::MatchType$package.Concat#[Ys]*/]
+// type Concat[Xs <: Tuple, +Ys <: Tuple] <: Tuple = Xs match
+//   case EmptyTuple => Ys
+//   case x *: xs => x *: Concat[xs, Ys]
 
